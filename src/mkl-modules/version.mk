0cdd93a9b8b19a4bb1ff5529660720c927c38d92
Jim Hayes <jhayes@sdsc.edu>
2014-09-02 19:13:58 -0700

    Use MKL version instead of intel version for MKL modulefile.  Correct MKL modulefile installation to applications instead of compilers.

--- src/mkl-modules/version.mk
+++ src/mkl-modules/version.mk
@@ -1,4 +1,7 @@
-NAME    = mkl-modules
-VERSION = 2013_sp1.2.144
-RELEASE = 0
-RPM.EXTRAS         = AutoReq:No
+NAME       = mkl-modules
+VERSION    = 11.1.2.144
+RELEASE    = 0
+
+INTELVERS  = 2013_sp1.2.144
+
+RPM.EXTRAS = AutoReq:No
