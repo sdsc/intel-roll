c461e4e95eb46436c50dc1a207de7ff4f82d6424
Trevor Cooper <tcooper@sdsc.edu>
2014-10-14 16:57:08 -0700

    APS-23 - Update intel compiler version to 2015.0.090

--- src/intel-compilers/version.mk
+++ src/intel-compilers/version.mk
@@ -1,40 +1,40 @@
 NAME              = intel-compilers
-VERSION           = 2013_sp1.2.144
-RELEASE           = 2
+VERSION           = 2015.0.090
+RELEASE           = 0
 PKGROOT           = /opt/intel
 
 SRC_SUBDIR        = intel-compilers
 
 SOURCE_NAME       = l_ccompxe
-SOURCE_SUFFIX     = tar.gz
+SOURCE_SUFFIX     = tgz
 SOURCE_VERSION    = $(VERSION)
 SOURCE_PKG        = $(SOURCE_NAME)_$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR        = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 L_FCOMPXE_NAME    = l_fcompxe
-L_FCOMPXE_SUFFIX  = tar.gz
+L_FCOMPXE_SUFFIX  = tgz
 L_FCOMPXE_VERSION = $(VERSION)
 L_FCOMPXE_PKG     = $(L_FCOMPXE_NAME)_$(L_FCOMPXE_VERSION).$(L_FCOMPXE_SUFFIX)
 L_FCOMPXE_DIR     = $(L_FCOMPXE_PKG:%.$(L_FCOMPXE_SUFFIX)=%)
 
 L_IPP_NAME        = l_ipp
-L_IPP_SUFFIX      = tar.gz
-L_IPP_VERSION     = 8.1.0.144
+L_IPP_SUFFIX      = tgz
+L_IPP_VERSION     = 8.2.0.090
 L_IPP_PKG         = $(L_IPP_NAME)_$(L_IPP_VERSION).$(L_IPP_SUFFIX)
 L_IPP_DIR         = $(L_IPP_PKG:%.$(L_IPP_SUFFIX)=%)
 
 L_MKL_NAME        = l_mkl
-L_MKL_SUFFIX      = tar.gz
-L_MKL_VERSION     = 11.1.2.144
+L_MKL_SUFFIX      = tgz
+L_MKL_VERSION     = 11.2.0.090
 L_MKL_PKG         = $(L_MKL_NAME)_$(L_MKL_VERSION).$(L_MKL_SUFFIX)
 L_MKL_DIR         = $(L_MKL_PKG:%.$(L_MKL_SUFFIX)=%)
 
 L_TBB_NAME        = l_tbb
-L_TBB_SUFFIX      = tar.gz
-L_TBB_VERSION     = 4.2.3.144
+L_TBB_SUFFIX      = tgz
+L_TBB_VERSION     = 4.3.0.090
 L_TBB_PKG         = $(L_TBB_NAME)_$(L_TBB_VERSION).$(L_TBB_SUFFIX)
 L_TBB_DIR         = $(L_TBB_PKG:%.$(L_TBB_SUFFIX)=%)
 
-TAR_GZ_PKGS = $(SOURCE_PKG) $(L_FCOMPXE_PKG) $(L_IPP_PKG) $(L_MKL_PKG) $(L_TBB_PKG)
+TGZ_PKGS = $(SOURCE_PKG) $(L_FCOMPXE_PKG) $(L_IPP_PKG) $(L_MKL_PKG) $(L_TBB_PKG)
 
 RPM.EXTRAS        = AutoReq:No
