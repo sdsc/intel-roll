1c2b5d10bb697c1782fcf9ef4bf1cadc01717595
Trevor Cooper <tcooper@sdsc.edu>
2014-10-17 14:15:46 -0700

    Use annotated tag(s) and version.sh for roll versioning

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = intel
-RELEASE = 0
-COLOR   = orange
+ROLLNAME	= intel
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR		= orange
 
 REDHAT.ROOT = $(PWD)
