977876665ed0e960d0b480f5b8a4741b06f4ee59
root <root@comet-devel.sdsc.edu>
2015-08-22 20:01:04 -0700

    more additions for multiple mkl modules

--- src/mkl-modules/version.mk
+++ src/mkl-modules/version.mk
@@ -9,6 +9,10 @@ VERSION_SRC = $(REDHAT.ROOT)/src/intel-compilers/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-EXTRA_MODULE_VERSIONS = 2015.2.164
+VERSION2_SRC = $(REDHAT.ROOT)/src/intel-compilers-2015/version.mk
+VERSION2_INC = version2.inc
+include $(VERSION2_INC)
+
+EXTRA_MODULE_VERSIONS = 11.2.2.164
 
 RPM.EXTRAS  = AutoReq:No
